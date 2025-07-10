f67ada68a5c8f508fe4100d56024db14aecfea2a
TomSinsel <sinseltom@gmail.com>
2025-07-10 10:31:28 +0200

    Updated the main_radiomics.oy adnt eh readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This repository provides a Python-based tool for extracting radiomics features from a folder of DICOM files using PyRadiomics, and saving the results into a CSV file. It is also integrated with RabbitMQ for asynchronous task handling or pipeline integration.
+This repository provides a Python-based tool for extracting radiomics features from a folder of DICOM files using PyRadiomics, and saving the results into a CSV file. It is also integrated with RabbitMQ for spipeline integration.
 
 Important requirements for running pyradiomics:
 -Python 3.9 (Python 3.7–3.9 are supported; Python 3.10+ is not supported by PyRadiomics)
@@ -7,8 +7,5 @@ Important requirements for running pyradiomics:
 Important Notes:
 -Large ROIs (e.g., whole-body outlines) can cause very long computation times due to voxel-wise feature calculations.
 
-
-Radiomics extraction is done using default PyRadiomics parameters; adjust the settings in the params.yaml file to get the desired settings.
-
-License
-MIT License
+Radiomics extraction is done using default PyRadiomics parameters; adjust the settings in the params.yaml file to get the desired settings. For more information about this go to this 
+webpage: https://pyradiomics.readthedocs.io/en/latest/index.html or go to the Github repository.
--- main_radiomics.py
+++ main_radiomics.py
@@ -26,16 +26,16 @@ class radiomics_class:
         
     def convert_DCM(self, data_folder):
         """This method makes from the dcm data nifti data. rtdose and rtplan can be in the folder but will not be used."""
+        logging.info("Converting dicom to nifti...")
         
-        # Find the rtsttruct in the dicomdata folder this file will be used to create the nifti data.
+        # Find the rtsttruct in the dicomdata folder this file will be used to create the nifti data.   
         try:
             for file in os.listdir(data_folder):
                 file_path = os.path.join(data_folder, file)
                 ds = pydicom.dcmread(file_path, stop_before_pixels=True)
                 
                 if ds.Modality == "RTSTRUCT":
-                    self.rtstruct_path = file_path
-                    
+                    self.rtstruct_path = file_path             
                     logging.info(f"RTstruct has been found and is at {self.rtstruct_path}")
                     break
         
@@ -66,11 +66,11 @@ class radiomics_class:
         
     def get_results(self):
         """Applies the pyradiomics package to the nifti data to get the radiomics results"""    
-    
+        logging.info("Calculating the radiomics features...")
+        
         try:
             image_path = os.path.join(self.nifti_output_folder, f"{self.image_file}.gz")
-            extractor = radiomics.featureextractor.RadiomicsFeatureExtractor(self.settings)
-            
+            extractor = radiomics.featureextractor.RadiomicsFeatureExtractor(self.settings)    
             self.result_dict = {}
             
             for file in os.listdir("niftidata"):
@@ -80,7 +80,6 @@ class radiomics_class:
                     continue
                     
                 result = extractor.execute(image_path, file_path)
-                
                 self.result_dict[file] = result
                 
             logging.info("All the radiomics results are completed")
@@ -90,16 +89,15 @@ class radiomics_class:
         
     def save_results(self, data_folder):
         """This saves the radiomics results into a csv file. It uses the ID of the patient in the title of the CSV file"""
+        logging.info("Saving the radiomics results...")
         
         try:                          
             ds = pydicom.dcmread(self.rtstruct_path, stop_before_pixels=True)
             id = ds.PatientID
-            
             result_path = os.path.join(data_folder, f"radiomics_results_{id}.csv")
             
             with open(result_path, 'w', newline='') as csvfile:
                 fieldnames = list(next(iter(self.result_dict.values())).keys())
-                
                 writer = csv.DictWriter(csvfile, fieldnames=['id'] + fieldnames)
                 writer.writeheader()
 
